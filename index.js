e6cd242f360e606f50f2db8f99c3e5db0beabe63
PaolaP <paolaperez8@gmail.com>
2021-11-08 23:25:35 -0500

    doesn't output the right thing transaction

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let balance = 500.00;
+const myAccount = new Account('billybob');
 
 class Transaction {
 
@@ -24,7 +25,7 @@ class Withdrawal extends Transaction {
   }
 
 }
-const myAccount = new Account("snow-patrol");
+//const myAccount = new Account("snow-patrol");
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
@@ -37,7 +38,7 @@ t2.commit();
 console.log('Transaction 2:', t2);
 
 
-t3 = new Deposit(120.00);
+t3 = new Deposit(120.00, myAccount);
 t3.commit();
 console.log('Transaction 3:', t3);
 console.log('Balance:', balance);
